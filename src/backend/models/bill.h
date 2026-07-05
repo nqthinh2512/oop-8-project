d38740efae023c52072657d7e0861d2461edc37d
nqthinh2512 <quozthiz1200@gmail.com>
2026-07-05 16:35:10 +0700

    Revert "Sửa sườn, thêm file"

--- src/backend/models/bill.h
+++ src/backend/models/bill.h
@@ -4,17 +4,12 @@
 #include <QString>
 #include <QDate>
 
-class Bill {
-private:
+struct Bill {
     int id;
     QString billName;       // Tên hóa đơn (Tiền điện, Tiền nước...)
     double amount;          // Số tiền cần đóng
     QDate dueDate;          // Hạn chót thanh toán
-    bool isPaid;           // Trạng thái: true (đã đóng) / false (chưa đóng)
-public:
-    Bill();
-    Bill(int n_id, const QString &n_billName, double n_amount, const QDate &n_dueDate, bool n_isPaid);
-    //đừng viết trực tiếp logic cho các hàm ở đây, đi qua .cpp để viết đi.
+    bool isPaid;            // Trạng thái: true (đã đóng) / false (chưa đóng)
 };
 
 #endif // BILL_H
--- src/backend/models/category.cpp
+++
@@ -1,6 +0,0 @@
-#include "category.h"
-
-Category::Category(): id(0), name(""), iconPath(""){}
-
-Category::Category(int n_id, const QString& n_name, const QString& n_path)
-    : id(n_id), name(n_name), iconPath(n_path){}
